a0833fcd55b0a5db8d9acb59e291915648b0bdb3
hujingting <brucehjt@gmail.com>
2024-07-14 20:40:21 +0800

    practice bubble sorting

--- codes/java/chapter_sorting_practice/insertion_sort.java
+++ codes/java/chapter_sorting_practice/insertion_sort.java
@@ -12,7 +12,7 @@ public class insertion_sort {
     static void insertionSort(int[] nums) {
         //初始状态下，数组的第 1 个元素已完成排序
         for(int i = 1; i < nums.length; i++) {
-            //选取数组的第 2 个元素作为 base ，将其插入到正确位置后，数组的前 2 个元素已排序。
+            //选取数组的第 2 个元素作为 base ，将其插入到正确位置
             int base = nums[i], j = i - 1;
             // 内循环：将 base 插入到已排序区间[0, i-1] 中的正确位置
             while (j >=0 && nums[j] > base) {
