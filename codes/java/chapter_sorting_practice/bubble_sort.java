4a19c506c4bf7791f0f99938ed7458f1ccc8546f
hujingting <brucehjt@gmail.com>
2024-07-15 23:15:28 +0800

    add quick sort algo

--- codes/java/chapter_sorting_practice/bubble_sort.java
+++ codes/java/chapter_sorting_practice/bubble_sort.java
@@ -12,40 +12,26 @@ public class bubble_sort {
 
     /* 冒泡排序（标志优化） */
     static void bubbleSortWithFlag(int[] nums) {
-
+        // 外循环：未排序区间为 [0, i]
         for (int i = nums.length - 1; i > 0; i--) {
             boolean flag = false;
+            // 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端
             for (int j = 0; j < i; j++) {
                 if (nums[j] > nums[j+1]) {
                     // 交换 nums[j] 与 nums[j+1]
                     int temp = nums[j];
                     nums[j] = nums[j+1];
                     nums[j+1] = temp;
+                    //记录交换元素
                     flag = true;
                 }
             }
 
+            //此轮“冒泡”未交换任何元素，直接跳出
             if (!flag) {
                 break;
             }
         }
-
-        // 外循环：未排序区间为 [0, i]
-        // for (int i = nums.length - 1; i > 0; i--) {
-        //     boolean flag = false; // 初始化标志位
-        //     // 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端
-        //     for (int j = 0; j < i; j++) {
-        //         if (nums[j] > nums[j + 1]) {
-        //             // 交换 nums[j] 与 nums[j + 1]
-        //             int tmp = nums[j];
-        //             nums[j] = nums[j + 1];
-        //             nums[j + 1] = tmp;
-        //             flag = true; // 记录交换元素
-        //         }
-        //     }
-        //     if (!flag)
-        //         break; // 此轮“冒泡”未交换任何元素，直接跳出
-        // }
     }
 
     public static void main(String[] args) {
